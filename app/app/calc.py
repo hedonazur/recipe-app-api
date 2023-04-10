047a7262051d2ed9153e7adc6714776a04b9cff3
carol <hedonazur@hotmail.com>
2023-04-10 17:26:13 -0500

    added django settings for db, tests calc, django managment command and its tests

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,9 +1,12 @@
 """
 Calculator functions
 """
+
+
 def add(x, y):
     "adds x and y and return result"
     return x + y
 
-def subtract(x,y):
+
+def subtract(x, y):
     return x - y
--- app/app/tests.py
+++ app/app/tests.py
@@ -6,13 +6,14 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc module"""
     def test_add_numbers(self):
         """ Test adding numbers"""
-        res = calc.add(1,2)
+        res = calc.add(1, 2)
         self.assertEqual(res, 3)
 
     def test_subtract(self):
-        res = calc.subtract(7,4)
+        res = calc.subtract(7, 4)
         self.assertEqual(res, 3)
