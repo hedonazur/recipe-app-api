7c1b79a59632d050d2aee70d6c59fbb8206b347a
carol <hedonazur@hotmail.com>
2023-04-20 20:13:44 -0500

    implement create user API

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -30,7 +30,7 @@ class PublicUserApiTests(TestCase):
             'password': 'testpass123',
             'name': 'Test Name',
         }
-        rest = self.client.post(CREATE_USER_URL, payload)
+        res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         user = get_user_model().objects.get(email=payload['email'])
@@ -60,7 +60,7 @@ class PublicUserApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
-            email=payload̈́['email']
+            email=payload['email']
         ).exists()
         self.assertFalse(user_exists)
 
